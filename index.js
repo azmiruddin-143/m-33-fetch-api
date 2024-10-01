87ab4408ca3bf636729463d4202824090b10fdc9
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-10-01 11:36:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// document.getElementById("clickbtn").addEventListener("click", () => {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(peb => peb.json())
+//     .then(data => console.log(data))
+
+// })
+
+// others object or arry //
+
+// function liader() {
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//         .then(peb => peb.json())
+//         .then(data => refarane(data))
+// }
+
+// function refarane(transfer) {
+//     for(let man of transfer){
+//         console.log(man);
+//     }
+// }
